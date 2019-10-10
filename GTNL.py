9b2bab59055dfdaacce9d5551c3801064b1fdd85
JacksonJ01 <56162607+JacksonJ01@users.noreply.github.com>
2019-10-09 23:22:41 -0400

    Create GTNL.py

---
+++ GTNL.py
@@ -0,0 +1,35 @@
+# Jared Jackson
+# 10/09/10
+# This is going to be like the previous game
+# where the user has to guess the correct number
+#only this time they will get unlimited guesses
+# In this exercise i want to type most of it here, on github
+# I want to do this to challenge myself even though it was not recomended
+from random import *
+rn = randint
+
+
+def guess():
+    
+    
+
+input("CLICK HERE AND PRESS ENTER TO ACTIVATE")
+print("""Woah, it's you again...
+I really didn't expect you to be back so soon...
+Especially after how bad you lost last time!!!
+You need a hopital?, because you just got burned!!!!!\n""")
+
+name = input("""Anyhow, I'm bad with names, so you'll have to remind me of your name.
+>>>""")
+print("Ahh, yes. I remember now.")
+print("Heh.")
+print(f"So, {name}, I want you to play another game.")
+print("And by another game, I mean is the same game.")
+print("Yes, the infamous \"Guessing The Numbers\" game, but this time you will be allow infinite guesses.")
+input("""Sounds fun, right?
+>>>""")
+print("Too bad, I don't give a rats donkey.\n")
+print("Alrighty, here we go\n")
+
+input("PRESS ENTER TO START THE GAME\n")
+
